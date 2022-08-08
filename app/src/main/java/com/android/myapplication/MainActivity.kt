4af9f032100e513672c96e732bac2e80dd961604
XTTX <3028856894@qq.com>
2022-08-08 21:52:13 +0800

    feat: 测试提交

--- app/src/main/java/com/android/myapplication/MainActivity.kt
+++ app/src/main/java/com/android/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.android.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"hello world", Toast.LENGTH_SHORT).show()
     }
 }
