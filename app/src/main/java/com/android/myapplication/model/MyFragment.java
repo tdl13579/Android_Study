c10ce7909e21b3653ffa0647d3efde213e20b719
XTTX <3028856894@qq.com>
2022-09-04 21:46:52 +0800

    加载动态fragment

---
+++ app/src/main/java/com/android/myapplication/model/MyFragment.java
@@ -0,0 +1,27 @@
+package com.android.myapplication.model;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.myapplication.R;
+
+public class MyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment_myfragment,null);
+       return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
