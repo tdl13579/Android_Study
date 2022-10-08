4bcbf7496c1d0de8d3e1b21374a3f511b341a852
XTTX <3028856894@qq.com>
2022-10-08 21:44:31 +0800

    复习

--- app/src/main/java/com/android/myapplication/kotlinStudy/Kotlin复习.kt
+++ app/src/main/java/com/android/myapplication/kotlinStudy/Kotlin复习.kt
@@ -5,10 +5,49 @@ class Stu2(var name: String,var age: Int){
     operator fun component1() = name // 需要这样声明才可以用作结构赋值
     operator fun component2() = age
 }
+// 密封类
+sealed class Bar
+class Test(var name: String) : Bar()
+class Test2(var age: Int) :Bar()
+// 嵌套类
+class Test3{
+    var name = "Tom"
+    class Nested{
+        fun foo() = 2 // Test3.Nested().foo()
+    }
+    // 内部类
+    inner class InnerTest4{
+        fun foo() = name // Test3().InnerTest4.foo()
+    }
+}
 
 enum class Color {
     RED, BLACK, BLUE, GREEN, WHITE
 }
+enum class Color2(var color:String){
+    Red("red"),
+    Blue("blue")
+}
+// 构造函数
+class Person constructor(val name: String,val age: Int){
+    val hobby = "爱好"
+    fun sayHobby(){
+        println("$hobby")
+    }
+    init {
+        println("name is $name")
+    }
+    constructor(name: String,   age: Int,hello: String) : this(name,age){
+        println("$hello")
+    }
+    inner class InnerPerson{
+        fun innerHobby(){
+            println("$hobby")
+        }
+    }
+}
+
+
 
 fun main() {
     /**
@@ -51,6 +90,22 @@ fun main() {
     var (m,n) = Stu2("Jerry",3) // 不是数据类不支持直接解构复制，需要用component1,2 这样按顺序来声明
     println("$x--$y") // Mike--2
     println("$m--$n") // Jerry--3
+
+    var list = mutableListOf("hello","world",1,2)
+    for(item in list){
+        println(item)
+    }
+    for(index in list.indices){
+        println("$index -- ${list[index]}")
+    }
+    var map = mutableMapOf<String,Any>("hello" to "你好","world" to "世界","1" to 1)
+    for ((key,value) in map){
+        println("$key -- $value")
+    }
+    fun String.hello():String{
+        return "hello"
+    }
+    println("str".hello())
 
 
 }
---
+++ app/src/main/java/com/android/myapplication/kotlinStudy/协程.kt
@@ -0,0 +1,4 @@
+package com.android.myapplication.kotlinStudy
+
+class Coroutines {
+}
